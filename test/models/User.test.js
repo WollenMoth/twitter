3cea5bd56730477d7100fb54531ef54f035e84f5
André Díaz <andre290800@gmail.com>
2022-04-19 14:06:42 -0500

    automatically fill dateCreated and lastUpdate

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -2,20 +2,13 @@ const User = require("../../app/models/User");
 
 describe("Unit Tests for User class", () => {
   test("Create an User object", () => {
-    const user = new User(
-      1,
-      "wollenmoth",
-      "André",
-      "Bio",
-      "dateCreated",
-      "lastUpdated"
-    );
+    const user = new User(1, "wollenmoth", "André", "Bio");
 
     expect(user.id).toBe(1);
     expect(user.username).toBe("wollenmoth");
     expect(user.name).toBe("André");
     expect(user.bio).toBe("Bio");
-    expect(user.dateCreated).toBe("dateCreated");
-    expect(user.lastUpdated).toBe("lastUpdated");
+    expect(user.dateCreated).not.toBeUndefined();
+    expect(user.lastUpdated).not.toBeUndefined();
   });
 });
