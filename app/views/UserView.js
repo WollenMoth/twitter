972c9a591dfce232667483f0db704d01b2557b8e
André Díaz <andre290800@gmail.com>
2022-04-19 15:02:03 -0500

    validate properties

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -1,6 +1,11 @@
 class UserView {
   static createUser(payload) {
     if (!payload) return { error: "El payload no existe" };
+
+    const { id, username, name } = payload;
+
+    if (typeof(id) !== "number" || typeof(username) !== "string" || typeof(name) !== "string")
+      return { error: "Las propiedades del payload necesitan tener un valor válido" };
   }
 }
 
