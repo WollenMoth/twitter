ade3cc9a41e0e2904a5f677b2feab2b663c6fd4e
André Díaz <andre290800@gmail.com>
2022-04-19 14:31:53 -0500

    getInfo

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -9,4 +9,13 @@ describe("Unit Tests for UserService class", () => {
     expect(user.name).toBe("André");
     expect(user.bio).not.toBeUndefined();
   });
+  test("Get all user data in a list", () => {
+    const user = UserService.create(1, "wollenmoth", "André");
+    const userInfoList = UserService.getInfo(user);
+
+    expect(userInfoList[0]).toBe(1);
+    expect(userInfoList[1]).toBe("wollenmoth");
+    expect(userInfoList[2]).toBe("André");
+    expect(userInfoList[3]).not.toBeUndefined();
+  });
 });
